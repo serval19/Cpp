da6741f855b0985711547429c05495f07e32c2de
Diwa Tomy <144988434+serval19@users.noreply.github.com>
2025-08-25 00:35:29 +0530

    Create typeconversion.cpp

---
+++ typeconversion.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+using namespace std;
+int main(){
+    char c='Z';
+    cout << "type conversion" << endl;
+    cout << (int)c+10 << endl;
+    float index=5.9675;
+    cout << int(index);
+    return 0;
+}
